2ab307097a0a7a733765df0e45db90b46dae7174
JunYamaguchi <jun.smith.y@gmail.com>
2019-01-04 11:51:11 +0900

    add comment

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
+// should use "go run main.go user.go
+
 package main
 
-import "fmt"
-
-type User struct {
-	Name string
-	Age  int
-}
-
-func (user *User) SayHello() {
-	fmt.Println(user.Name, ": Hello.")
-}
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hogehoge")
