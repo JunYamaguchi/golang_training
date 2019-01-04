d870f1424202ca86733a7c2e2f4555e3edc5e815
JunYamaguchi <jun.smith.y@gmail.com>
2019-01-04 11:15:31 +0900

    add struct

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main(){
+type User struct {
+	Name string
+	Age  int
+}
+
+func main() {
 	fmt.Println("hogehoge")
+	yamada := User{Name: "Yamada", Age: 30}
+	fmt.Println(yamada.Name)
+	fmt.Println(yamada.Age)
 }
