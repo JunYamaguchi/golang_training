27337e8b2ca2349acdba33f7758cd94c4640b410
JunYamaguchi <jun.smith.y@gmail.com>
2019-01-06 17:03:02 +0900

    mapの値の存在チェックを追加

--- main.go
+++ main.go
@@ -18,5 +18,45 @@ func main() {
 	// map = make(map[key]value)
 
 	mockDB[1] = "hoge"
-	fmt.Println(mockDB)
+	fmt.Println("mockDB is", mockDB)
+	fmt.Println("mockDB[1] is ", mockDB[1])
+	fmt.Println("mockDB[2] is ", mockDB[2])
+	// 定義してない値を開こうとするとvalueのゼロ値が返る
+	//fmt.Println("mockDB[\"hoge\"] is", mockDB["hoge"])
+	// keyの方が合わないと開けないしコンパイルエラーが出る
+
+	var (
+		mockDBValue string
+		ok          bool
+	)
+
+	mockDBValue, ok = mockDB[1]
+	fmt.Println(mockDBValue, ok)
+
+	if ok {
+		fmt.Println("mockDB[1] exist check is True")
+	} else {
+		fmt.Println("mockDB[1] exits check is False")
+	}
+
+	mockDBValue, ok = mockDB[2]
+	fmt.Println(mockDBValue, ok)
+
+	if ok {
+		fmt.Println("mockDB[2] exist check is True")
+	} else {
+		fmt.Println("mockDB[2] exits check is False")
+	}
+
+	delete(mockDB, 1)
+	fmt.Println("mockDB[1] is deleted")
+
+	mockDBValue, ok = mockDB[1]
+	fmt.Println(mockDBValue, ok)
+
+	if ok {
+		fmt.Println("mockDB[1] exist check is True")
+	} else {
+		fmt.Println("mockDB[1] exits check is False")
+	}
 }
